4fad11f88eb677f72438f5da2dacd584cda275fc
shaul <shaul1991@gmail.com>
2025-03-10 22:15:53 +0900

    initialize Spring Boot service registry application with Gradle setup

---
+++ src/main/kotlin/com/shaul/serviceregistry/ServiceRegistryApplication.kt
@@ -0,0 +1,11 @@
+package com.shaul.serviceregistry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceRegistryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceRegistryApplication>(*args)
+}
